cdee9fb26adf22f7188877d53ff27240c957b0cb
Gregory Malecha <gregory@bluerock.io>
2025-02-08 23:47:07 -0500

    More hacking...

---
+++ content/tech_reports/developing-with-formal-methods.md
@@ -0,0 +1,10 @@
+---
+title: Developing with Formal Methods and BedRock Systems
+tags:
+    - tech_reports
+where: IEEE S&P
+download_link: my-download-link
+year: 2022
+---
+
+We present our work verifying industrial C++ code.
--- eleventy.config.js
+++ eleventy.config.js
@@ -6,7 +6,6 @@ export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets/bluerock-dark.svg");
     eleventyConfig.addPassthroughCopy("assets/bluerock-cube.svg");
 
-
     eleventyConfig.addGlobalData("siteName", "BlueRock Verification");
     eleventyConfig.addGlobalData("companyName", "BlueRock Security Inc");
     eleventyConfig.addGlobalData("copyrightYear", function() { return new Date().getUTCFullYear(); });
