12d7c73b53711b86ceadb60feeab557a56497eea
Gregory Malecha <gregory@bluerock.io>
2025-02-09 23:12:48 -0500

    Adding the favicon.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,10 @@
-export default function(eleventyConfig) {
+export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css/styles.css");
     eleventyConfig.addPassthroughCopy("js/scripts.js");
-    eleventyConfig.addPassthroughCopy("assets/bluerock.svg");
-    eleventyConfig.addPassthroughCopy("assets/bluerock-white.svg");
-    eleventyConfig.addPassthroughCopy("assets/bluerock-dark.svg");
-    eleventyConfig.addPassthroughCopy("assets/bluerock-cube.svg");
+    eleventyConfig.addPassthroughCopy("assets/*.svg");
+    eleventyConfig.addPassthroughCopy("assets/favicon.ico");
 
     eleventyConfig.addGlobalData("siteName", "BlueRock Verification");
     eleventyConfig.addGlobalData("companyName", "BlueRock Security Inc");
-    eleventyConfig.addGlobalData("copyrightYear", function() { return new Date().getUTCFullYear(); });
+    eleventyConfig.addGlobalData("copyrightYear", function () { return new Date().getUTCFullYear(); });
 };
