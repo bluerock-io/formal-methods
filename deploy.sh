6c677c4e7c02925d7cfcae97777a714abb49cb70
Gregory Malecha <gregory@bluerock.io>
2025-06-29 18:11:55 -0400

    Adding the deploy script.

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+rm -rf _github
+git clone --depth=1 -b github-pages git@gitlab.com:bedrocksystems/formal-methods/website.git _github
+(cd _github; git checkout -b github-pages; git rm -rf *)
+npx @11ty/eleventy --input=. --output=_github --pathprefix='formal-methods/'
+(cd _github; git add .)
